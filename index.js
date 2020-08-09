9a1048ca3ff51e63636cfc4f50293b4650f9fc5c
Hilton Gonçalves <hiltong8@gmail.com>
2020-08-09 20:15:15 -0300

    pendente apenas inserir data-hora no put

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import { promises as fs } from 'fs';
 import gradesRouter from './routes/grades.js';
+import bodyParser from 'body-parser';
+
 const { readFile, writeFile } = fs;
 const app = express();
 global.fileName = 'grades.json';
+
+// configure the app to use bodyParser()
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 app.use('/grades', gradesRouter);
 app.listen(34567, async () => {
--- routes/grades.js
+++ routes/grades.js
@@ -1,28 +1,96 @@
 import express from 'express';
 import { promises as fs, write } from 'fs';
 import { globalAgent } from 'http';
+import { stringify } from 'querystring';
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
 
 const router = express.Router();
 const { readFile, writeFile } = fs;
+
+// 5 - SOMA TOTAL OK
+router.get('/getsoma/', async (req, res) => {
+  try {
+    const aluno = req.body.student;
+    const materia = req.body.subject;
+    const data = JSON.parse(await readFile(global.fileName));
+    const grade = data.grades.filter(
+      (grade) => grade.student === aluno && grade.subject === materia
+    );
+
+    var sum = grade.reduce(function (previousValue, currentValue) {
+      return previousValue + currentValue.value;
+    }, 0);
+    res.send(` Total: ${sum}`);
+
+    res.end();
+  } catch (error) {
+    console.log(error);
+    res.end();
+  }
+});
+
+// 6- MEDIA OK
+router.get('/getmedia/', async (req, res) => {
+  try {
+    const tipo = req.body.type;
+    const materia = req.body.subject;
+    const data = JSON.parse(await readFile(global.fileName));
+    let grade = data.grades.filter(
+      (grade) => grade.type == tipo && grade.subject == materia
+    );
+    let sum = grade.reduce(function (previousValue, currentValue) {
+      return previousValue + currentValue.value;
+    }, 0);
+    let media = sum / grade.length;
+    res.send(` Média: ${media}`);
+
+    res.end();
+  } catch (error) {
+    console.log(error);
+    res.end();
+  }
+});
+
+// 7 - TOP 3 OK
+router.get('/top3/', async (req, res) => {
+  try {
+    const tipo = req.body.type;
+    const materia = req.body.subject;
+    const data = JSON.parse(await readFile(global.fileName));
+    const grade = data.grades.filter(
+      (grade) => grade.type == tipo && grade.subject == materia
+    );
+    // ordena
+    grade.sort((a, b) => {
+      return b.value - a.value;
+    });
+
+    res.send(grade.slice(0, 3));
+
+    res.end();
+  } catch (error) {
+    console.log(error);
+    res.end();
+  }
+});
 
 // 1 - CRIAR GRADE
 router.post('/', async (req, res) => {
   try {
     let insert = req.body;
-    console.log(insert);
     const data = JSON.parse(await readFile(global.fileName));
-    // let temp = {
-    //   nextId: data.nextId++,
-    //   grades: {
-    //     student: insert.student,
-    //     subject: insert.subject,
-    //     type: insert.type,
-    //     value: insert.value,
-    //   },
-    // };
-    //data.push(insert);
-    //console.log(temp);
-    res.send(insert);
+    let grade = {
+      id: data.nextId,
+      student: insert.student,
+      subject: insert.subject,
+      type: insert.type,
+      value: parseInt(insert.value),
+    };
+
+    data.nextId = data.nextId + 1;
+    data.grades.push(grade);
+    await writeFile(global.fileName, JSON.stringify(data));
+    res.send('objeto criado');
     res.status(201);
     res.end();
   } catch (error) {
@@ -31,25 +99,21 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', async (req, res) => {
+// 2 - ATUALIZAR
+router.patch('/:id', async (req, res) => {
+  const body = req.body;
   try {
-    let update = req.body;
-
-    console.log(update);
-
     const data = JSON.parse(await readFile(global.fileName));
-    const grade = data.grades.filter((grade) => grade.id == req.params.id);
-    grade.student = update.student;
-    grade.subject = update.subject;
-    grade.type = update.type;
-    grade.value = update.value;
+    const index = data.grades.findIndex(
+      (grade) => grade.id === parseInt(req.params.id)
+    );
 
-    let temp = {
-      nextId: data.nextId++,
-      grades: grade,
-    };
+    data.grades[index].subject = body.subject;
+    data.grades[index].type = body.type;
+    data.grades[index].value = parseInt(body.value);
 
-    //await writeFile(global.fileName, JSON.stringify(grade, null, 2));
+    await writeFile(global.fileName, JSON.stringify(data, null, 2));
+    res.send(data.grades[index]);
     res.end();
   } catch (error) {
     console.log(error);
@@ -64,8 +128,6 @@ router.delete('/:id', async (req, res) => {
     const grade = data.grades.filter(
       (grade) => grade.id !== parseInt(req.params.id)
     );
-    // console.log(req.params.id);
-    //console.log(grade);
 
     let temp = {
       nextId: data.nextId++,
@@ -91,22 +153,4 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-// 5 - SOMA TOTAL
-router.get('/totalBySubjct', async (req, res) => {
-  try {
-    const data = JSON.parse(await readFile(global.fileName));
-    const grade = data.grades.filter(
-      (grade) => grade.id !== parseInt(req.params.id)
-    );
-    let temp = {
-      nextId: data.nextId++,
-      grades: grade,
-    };
-    await writeFile(global.fileName, JSON.stringify(temp, null, 2));
-    res.end();
-  } catch (error) {
-    console.log(error);
-    res.end();
-  }
-});
 export default router;
